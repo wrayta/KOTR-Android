7b90fce3c60f7c4fb68aa7a1f4a35d71c4f311a6
Jeanine Encizo <wrayta@hotmail.com>
2017-05-23 13:45:02 -0400

    Styled main menu and replay dialog box

--- app/src/main/java/Messages/ReplayDialogFragment.java
+++ app/src/main/java/Messages/ReplayDialogFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
 import android.view.Window;
 import android.widget.Button;
 
@@ -20,13 +21,19 @@ import com.example.thomas.kotr_android.R;
 
 public class ReplayDialogFragment extends DialogFragment {
 
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup oontainer,
+//                             Bundle savedInstanceState) {
+//        return getActivity().getLayoutInflater().inflate(R.layout.)
+//    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         Bundle bundle = getArguments();
         int score = bundle.getInt("SCORE");
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AppAlertTheme);
 
         // Add the buttons
         builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -53,29 +60,32 @@ public class ReplayDialogFragment extends DialogFragment {
         // Create the AlertDialog
         Dialog dialog = builder.create();
 
-        // Make some UI changes for AlertDialog
-        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(final DialogInterface dialog) {
 
-                // Add or create your own background drawable for AlertDialog window
-                Window view = ((AlertDialog)dialog).getWindow();
-//                view.setBackgroundDrawableResource(R.drawable.your_drawable);
-
-                // Customize POSITIVE, NEGATIVE and NEUTRAL buttons.
-                Button positiveButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
-                positiveButton.setTextColor(getResources().getColor(R.color.colorPrimary));
-                positiveButton.setTypeface(Typeface.DEFAULT);
-//                positiveButton.setTypeface(Typeface.DEFAULT_BOLD);
-                positiveButton.invalidate();
-
-                Button neutralButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_NEUTRAL);
-                neutralButton.setTextColor(getResources().getColor(R.color.colorPrimary));
-                neutralButton.setTypeface(Typeface.DEFAULT);
-//                neutralButton.setTypeface(Typeface.DEFAULT_BOLD);
-                neutralButton.invalidate();
-            }
-        });
+//        // Make some UI changes for AlertDialog
+//        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+//            @Override
+//            public void onShow(final DialogInterface dialog) {
+//
+//                // Add or create your own background drawable for AlertDialog window
+//                Window view = ((AlertDialog)dialog).getWindow();
+////                view.setBackgroundDrawableResource(R.drawable.your_drawable);
+//
+//                // Customize POSITIVE, NEGATIVE and NEUTRAL buttons.
+//                Button positiveButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+////                positiveButton.setTextColor(getResources().getColor(R.color.colorPrimary));
+//                positiveButton.setTypeface(Typeface.DEFAULT);
+//                positiveButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+////                positiveButton.setTypeface(Typeface.DEFAULT_BOLD);
+//                positiveButton.invalidate();
+//
+//                Button neutralButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_NEUTRAL);
+////                neutralButton.setTextColor(getResources().getColor(R.color.colorPrimary));
+//                neutralButton.setTypeface(Typeface.DEFAULT);
+//                neutralButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+////                neutralButton.setTypeface(Typeface.DEFAULT_BOLD);
+//                neutralButton.invalidate();
+//            }
+//        });
 
         return dialog;
     }
--- app/src/main/java/com/example/thomas/kotr_android/MainMenuActivity.java
+++ app/src/main/java/com/example/thomas/kotr_android/MainMenuActivity.java
@@ -1,11 +1,13 @@
 package com.example.thomas.kotr_android;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainMenuActivity extends FragmentActivity {
 
@@ -14,6 +16,9 @@ public class MainMenuActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        ImageView knightLogoView = (ImageView)findViewById(R.id.knightLogoView);
+
+        ((AnimationDrawable) knightLogoView.getBackground()).start();
         Button arcadeModeButton = (Button) findViewById(R.id.arcadeButton);
 
         arcadeModeButton.setOnClickListener(new View.OnClickListener() {
